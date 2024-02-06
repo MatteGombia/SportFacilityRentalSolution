007ea4ab020a3c7a24a0d4e03ac32697a5db37f8
MatteGombia <matteogombia@gmail.com>
2024-02-06 22:45:45 +0100

    added verify in controller test

--- src/test/java/booking/controllers/BookingControllerIntegrationTest.java
+++ src/test/java/booking/controllers/BookingControllerIntegrationTest.java
@@ -4,16 +4,12 @@ import booking.models.Booking;
 import booking.models.BookingRequest;
 import booking.models.BookingResponse;
 import booking.services.BookingService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tomcat.jni.User;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.convention.MatchingStrategies;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +17,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.*;
 
-
-import java.awt.print.Book;
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -48,7 +40,7 @@ public class BookingControllerIntegrationTest {
     private TestRestTemplate testRestTemplate;
 
     @MockBean
-    private BookingService reportMockService;
+    private BookingService bookingMockService;
 
     @Test
     public void testCreateBooking() throws JSONException {
@@ -58,7 +50,7 @@ public class BookingControllerIntegrationTest {
         String endpoint = "/booking";
         Booking serviceBooking = new Booking(1L,1L,1L,5, LocalDate.parse("2024-12-31"), LocalTime.parse("14:30:00"), LocalTime.parse("15:30:00"));
 
-        when(reportMockService.saveBooking(any(Booking.class))).thenReturn(serviceBooking);
+        when(bookingMockService.saveBooking(any(Booking.class))).thenReturn(serviceBooking);
 
         ResponseEntity<String> responseEntity =
                 testRestTemplate.postForEntity(endpoint, bookingRequest, String.class);
@@ -71,6 +63,8 @@ public class BookingControllerIntegrationTest {
         Assertions.assertEquals(jsonResponse.getString("timeStart"), expectedBookingResponse.getTimeStart().format(DateTimeFormatter.ISO_TIME).toString());
         Assertions.assertEquals(jsonResponse.getString("timeEnd"), expectedBookingResponse.getTimeEnd().format(DateTimeFormatter.ISO_TIME).toString());
 
+        verify(bookingMockService, times(1)).saveBooking(any(Booking.class));
+
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
     }
 
@@ -81,7 +75,7 @@ public class BookingControllerIntegrationTest {
         String endpoint = "/booking/1";
         Booking serviceBooking = new Booking(1L,1L,1L,5, LocalDate.parse("2024-12-31"), LocalTime.parse("14:30:00"), LocalTime.parse("15:30:00"));
 
-        when(reportMockService.getBookingById(any(Long.class))).thenReturn(serviceBooking);
+        when(bookingMockService.getBookingById(any(Long.class))).thenReturn(serviceBooking);
 
         ResponseEntity<String> responseEntity =
                 testRestTemplate.getForEntity(endpoint, String.class);
@@ -96,6 +90,7 @@ public class BookingControllerIntegrationTest {
         Assertions.assertEquals(jsonResponse.getString("timeEnd"), expectedBookingResponse.getTimeEnd().format(DateTimeFormatter.ISO_TIME).toString());
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        verify(bookingMockService, times(1)).getBookingById(any(Long.class));
     }
 
     @Test
@@ -107,7 +102,7 @@ public class BookingControllerIntegrationTest {
         List<Booking> serviceBookingList = new ArrayList<>();
         serviceBookingList.add(serviceBooking);
 
-        when(reportMockService.getBookingByUser(any(Long.class))).thenReturn(serviceBookingList);
+        when(bookingMockService.getBookingByUser(any(Long.class))).thenReturn(serviceBookingList);
 
         ResponseEntity<String> responseEntity =
                 testRestTemplate.getForEntity(endpoint, String.class);
@@ -122,6 +117,8 @@ public class BookingControllerIntegrationTest {
         Assertions.assertEquals(jsonResponse.getString("date"), expectedBookingResponse.getDate().toString());
         Assertions.assertEquals(jsonResponse.getString("timeStart"), expectedBookingResponse.getTimeStart().format(DateTimeFormatter.ISO_TIME).toString());
         Assertions.assertEquals(jsonResponse.getString("timeEnd"), expectedBookingResponse.getTimeEnd().format(DateTimeFormatter.ISO_TIME).toString());
+
+        verify(bookingMockService, times(1)).getBookingByUser(any(Long.class));
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
@@ -135,11 +132,10 @@ public class BookingControllerIntegrationTest {
         List<Booking> serviceBookingList = new ArrayList<>();
         serviceBookingList.add(serviceBooking);
 
-        when(reportMockService.getBookingByField(any(Long.class))).thenReturn(serviceBookingList);
+        when(bookingMockService.getBookingByField(any(Long.class))).thenReturn(serviceBookingList);
 
         ResponseEntity<String> responseEntity =
                 testRestTemplate.getForEntity(endpoint, String.class);
-        System.out.println(responseEntity.toString());
         JSONArray jsonResponseArray = new JSONArray(responseEntity.getBody());
         JSONObject jsonResponse = jsonResponseArray.getJSONObject(0);
 
@@ -152,51 +148,25 @@ public class BookingControllerIntegrationTest {
         Assertions.assertEquals(jsonResponse.getString("timeStart"), expectedBookingResponse.getTimeStart().format(DateTimeFormatter.ISO_TIME).toString());
         Assertions.assertEquals(jsonResponse.getString("timeEnd"), expectedBookingResponse.getTimeEnd().format(DateTimeFormatter.ISO_TIME).toString());
 
+        verify(bookingMockService, times(1)).getBookingByField(any(Long.class));
+
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
-
-
-    /*
 
     @Test
-    public void testDeleteValidField() {
-        Long fieldId = 1L;
+    public void testDeleteBooking() throws JSONException {
+        String endpoint = "/booking/1";
+        HttpEntity<String> entity = new HttpEntity<>(null, new HttpHeaders());
+        doNothing().when(bookingMockService).deleteBookingById(any(Long.class));
 
-        String endpoint = "/fields/{id}";
+        // Act
+        ResponseEntity<String> response = testRestTemplate.exchange(endpoint, HttpMethod.DELETE, entity, String.class);
 
-        ResponseEntity<String> responseEntity = testRestTemplate.exchange(
-                endpoint,
-                HttpMethod.DELETE,
-                null,
-                String.class,
-                fieldId);
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
 
-        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
+        verify(bookingMockService, times(1)).deleteBookingById(any(Long.class));
 
-
-
-        assertNull(responseEntity.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
-    */
-
-    /*@Test
-    public void testGetOneField() {
-        Long fieldId = 1L;
-
-        String endpoint = "/fields/{id}";
-
-        ResponseEntity<FieldResponse> responseEntity = testRestTemplate.exchange(
-                endpoint,
-                HttpMethod.GET,
-                null,
-                FieldResponse.class,
-                fieldId);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-
-       FieldResponse expectedFieldResponse = new FieldResponse(1L, "Football field", 20.50, 45,
-               "Mosta, Brown street 23", "Keys to the field are at watchman post");
-
-        assertThat(responseEntity.getBody()).isEqualTo(expectedFieldResponse);
-    }*/
 }
